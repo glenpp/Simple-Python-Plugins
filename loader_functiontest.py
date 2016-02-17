d85a63cd3456efde944900b63fe06b19e37af1b5
Glen Pit-Pladdy <glenpp@users.noreply.github.com>
2016-02-17 18:35:21 +0000

    Initial commit

---
+++ loader_functiontest.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+#
+# Example non-OO (Function) Plugin Loading
+#
+#    Copyright (C) 2016  Glen Pitt-Pladdy
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# See: https://www.pitt-pladdy.com/blog/_20160217-161338_0000_Simple_Python_Plugins/
+#
+
+
+import imp
+import sys
+import gc
+
+# Name of the plugin we are going to load
+name = 'plugin_functiontest'
+
+
+# Convenience memory display function - run garbage collect and print number of objects
+def collectprint ( stage=None ):
+	gc.collect()	# garbage collect
+	if stage != None:
+		print stage
+		print "\t#objects: %d" % len(gc.get_objects())
+	else:
+		print "number of objects: %d" % len(gc.get_objects())
+
+
+# load module
+collectprint( 'Before Loading' )
+info = imp.find_module ( name )	# search path optional and includes prefix, return (file, pathname, description)
+plugin = imp.load_module ( name, *info )	# returns object
+collectprint( 'Module Loaded' )
+
+# cleanup
+del info
+collectprint( 'Cleaned up module vars' )
+
+# run function
+print plugin.variable
+plugin.test ()
+collectprint( 'After test() function run' )
+
+# unload
+del plugin
+collectprint( 'Cleaned up module vars' )
+sys.modules.pop ( name )
+collectprint( 'Removed module' )
+
+
---
+++ plugin_classtest.py
@@ -0,0 +1,31 @@
+#
+# Example OO (Class) Plugin Loading
+#
+#    Copyright (C) 2016  Glen Pitt-Pladdy
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# See: https://www.pitt-pladdy.com/blog/_20160217-161338_0000_Simple_Python_Plugins/
+#
+
+
+class plugintest:	# this could be inherited
+	def __init__ ( self ):
+		print "Plugin: __init__()"
+	def test ( self ):
+		print "Plugin: test()"
+
+
+
